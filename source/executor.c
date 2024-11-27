7bcd18e8130606726678408f6a9873bc83ce32a8
Ahmet Diler <adiler@c3a9c1.42berlin.de>
2024-11-27 17:52:12 +0100

    pass env to executor

--- source/executor.c
+++ source/executor.c
@@ -6,7 +6,7 @@
 /*   By: adiler <adiler@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 16:58:25 by adiler            #+#    #+#             */
-/*   Updated: 2024/11/27 17:03:05 by adiler           ###   ########.fr       */
+/*   Updated: 2024/11/27 17:25:34 by adiler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,38 +25,40 @@ void ft_free_split(char **str)
 	free(str);
 }
 
-static char *find_command_in_path(char *cmd, char **env)
+static char *find_command_in_path(char *cmd)
 {
-	(void)env;
-	char *path;
-	char **paths;
-	char *full_path;
+    char *path;
+    char **paths;
+    char *full_path;
+    char *temp;
+    
     if (cmd[0] == '/' || cmd[0] == '.')
-		return (cmd);
-	
-	path = getenv("PATH");
-	paths = ft_split(path, ':');
-	if (!paths)
-		return (NULL);
-	for (int i = 0; paths[i]; i++)
-	{
-		full_path = ft_strjoin(paths[i], "/");
-		full_path = ft_strjoin(full_path, cmd);
-		if (access(full_path, F_OK) == 0)
-		{
-			ft_free_split(paths);
-			return (full_path);
-		}
-	}
-	ft_free_split(paths);
-	return (NULL);
+        return (cmd);
+    path = getenv("PATH");
+    paths = ft_split(path, ':');
+    if (!paths)
+        return (NULL);
+    for (int i = 0; paths[i]; i++)
+    {
+        temp = ft_strjoin(paths[i], "/");
+        full_path = ft_strjoin(temp, cmd);
+        free(temp);
+        if (access(full_path, F_OK) == 0)
+        {
+            ft_free_split(paths);
+            return (full_path);
+        }
+        free(full_path);
+    }
+    ft_free_split(paths);
+    return (NULL);
 }
 
 static int handle_child_process(char **args, char **env)
 {
     char *cmd_path;
     
-    cmd_path = find_command_in_path(args[0], env);
+    cmd_path = find_command_in_path(args[0]);
     if (!cmd_path)
         exit(127);  // Command not found
         
@@ -93,6 +95,8 @@ int execute_command(char **args, char **envp)
 {
     pid_t pid;
 
+	if(!args[0])
+		return (0);
     pid = fork();
     if (pid == -1)
         return (perror("fork"), 1);
