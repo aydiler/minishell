f5eda1ad48122e1251ad5f6fdb75ff14ca6d45db
Maarten Hoff <maahoff@c4a7c6.42berlin.de>
2024-12-05 17:00:32 +0100

    folders

--- source/main.c
+++ source/main.c
@@ -2,40 +2,40 @@
 
 int main(int argc, char **argv, char **envp)
 {
-    char    *input;
-    t_cmd   *cmd;
-    int     exit_status;
+	char	*input;
+	t_cmd	*cmd;
+	int		exit_status;
 
-    cmd = NULL;
-    exit_status = 0;
-    (void)argc;
-    (void)argv;
-    setup_signals();
-    load_history();
-    while (1)
-    {
-        input = readline("minishell$ ");
-        if (!input)
-        {
-            ft_putstr_fd("exit\n", 1);
-            break;
-        }
-        if (ft_strncmp(input, "exit", ft_strlen("exit")) == 0)
-        {
-            free(input);
-            break;
-        }
-        if(*input)
-        {
-            add_history(input);
-            save_history(input);
-        }
-        parser(input, &cmd);
-        print_struct(cmd);
-        if (cmd)
-            exit_status = execute_command(cmd->args, envp, signal_handler);
-        free(input);
-        free_all(cmd);
-    }
-    exit(exit_status);
+	cmd = NULL;
+	exit_status = 0;
+	(void)argc;
+	(void)argv;
+	setup_signals();
+	load_history();
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+		{
+			ft_putstr_fd("exit\n", 1);
+			break ;
+		}
+		if (ft_strncmp(input, "exit", ft_strlen("exit")) == 0)
+		{
+			free(input);
+			break ;
+		}
+		if (*input)
+		{
+			add_history(input);
+			save_history(input);
+		}
+		parser(input, &cmd);
+		print_struct(cmd);
+		if (cmd)
+			exit_status = execute_command(cmd->args, envp, signal_handler);
+		free(input);
+		free_all(cmd);
+	}
+	exit(exit_status);
 }
